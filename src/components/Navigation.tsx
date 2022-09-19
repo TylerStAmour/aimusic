31f8b432c11ae54ed4df01f0721d7b608f3b2a27
Tyler St-Amour <tylerstamour1@gmail.com>
2022-09-19 09:08:44 -0400

    Added mobile responsiveness

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,17 +2,17 @@ import './elements.css';
 import {tsParticles} from "tsparticles-engine";
 
 export default () => (
-    <div className={"flex justify-center mb-14 text-white pt-8"} id={"navbar"}>
-        <a href={"/accueil"} className={"navlink"}>
+    <div className={"grid grid-cols-2 gap-3 sm:flex sm:justify-center mb-14 text-white pt-8"} id={"navbar"}>
+        <a href={"/accueil"} className={"navlink w-min flex justify-self-center"}>
             Accueil
         </a>
-        <a href={"/impacte"} className={"navlink"}>
+        <a href={"/impacte"} className={"navlink w-min flex justify-self-center"}>
             Impacte
         </a>
-        <a href={"/experimente"} className={"navlink"}>
+        <a href={"/experimente"} className={"navlink w-min flex justify-self-center"}>
             Expérimente
         </a>
-        <a href={"/technologie"} className={"navlink"}>
+        <a href={"/technologie"} className={"navlink w-min flex justify-self-center"}>
             Technologie
         </a>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,22 +20,22 @@ export default () => {
                 <div className={"flex flex-col grow space-between"}>
                     <div>
                         <div className={"flex flex-col items-center"}>
-                            <p className={"text-4xl z-10 text-white"}>
+                            <p className={"text-xl sm:text-4xl z-10 text-white"}>
                                 L'INTELLIGENCE DE LA MUSIQUE
                             </p>
-                            <p className={"text-xl z-10 text-gray-400"}>
+                            <p className={"text-md sm:text-xl z-10 text-gray-400"}>
                                 Les réseaux neuronal qui génère de la musique
                             </p>
                         </div>
                         <div className={"flex flex-col mt-24 2xl:mt-36 items-center text-white"}>
-                            <p className={"max-w-3xl z-10"}>
+                            <p className={"max-w-3xl z-10 pl-4 pr-4"}>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet est in urna fringilla interdum. Cras quis tristique nisl. Sed ex nisl, convallis eget laoreet non, congue at quam. Sed porttitor lobortis aliquet. Aenean accumsan vel nibh pellentesque rutrum. Vivamus ultricies metus sapien, et dignissim nisl eleifend et. Quisque sed neque urna. Vestibulum congue ex ut diam placerat elementum.
                             </p>
                         </div>
                     </div>
                     <div className={"flex flex-row justify-center items-end grow text-white"}>
                         <div className={"flex grow flex-col"}>
-                            <div className={"flex justify-center items-end bg-gradient-to-b from-neutral-900 to-dark h-24"}>
+                            <div className={"flex justify-center items-end bg-gradient-to-b from-neutral-900 to-dark h-24 xl:h-48"}>
                                 <div className={"flex justify-center mb-6"}>
                                     <a onClick={handleClick} style={{ cursor: 'pointer' }} className={"flex flex-col items-center hover:opacity-75 hover:text-secondary hover:-translate-y-0.5 transition ease-in duration-100"}>
                                         <p className={"text-xl !text-gray-300"}>
@@ -55,20 +55,24 @@ export default () => {
             </div>
             <div className={"relative z-10"}>
                 <div className={"h-52 bg-dark"}/>
-                <div className={"h-36 bg-gradient-to-b from-dark to-neutral-900 "}/>
-                <p className={"bg-neutral-900 text-white text-3xl pt-12 flex justify-center"}>
+                <div className={"h-36 bg-gradient-to-b from-dark to-neutral-900"}/>
+                <p className={"bg-neutral-900 text-white text-3xl pt-12 xl:pt-18 flex justify-center"}>
                     Les Réseaux Neuronales
                 </p>
-                <div className={"grid grid-cols-2 gap-10 bg-neutral-900 pt-20 pl-20 pr-20 pb-24"}>
-                    <div ref={viewMoreRef}>
-                        <p className={"text-white text-2xl"}>C'est quoi un réseau neuronal?</p>
+                <div className={"flex flex-col sm:grid sm:grid-cols-2 sm:gap-10 bg-neutral-900 pt-20 pl-10 sm:pl-20 pr-10 sm:pr-20 pb-24"}>
+                    <div ref={viewMoreRef} className={"pb-12 sm:pb-0"}>
+                        <p className={"text-white text-xl sm:text-2xl"}>C'est quoi un réseau neuronal?</p>
                         <p className={"text-gray-400 mt-2"}>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet est in urna fringilla interdum. Cras quis tristique nisl. Sed ex nisl, convallis eget laoreet non, congue at quam. Sed porttitor lobortis aliquet. Aenean accumsan vel nibh pellentesque rutrum. Vivamus ultricies metus sapien, et dignissim nisl eleifend et. Quisque sed neque urna. Vestibulum congue ex ut diam placerat elementum.
                         </p>
                     </div>
-                    <div className={"flex h-52 self-center justify-self-center bg-neutral-800 rounded-lg"}>
+                    <div className={"flex h-52 w-full sm:w-auto self-center justify-self-center bg-neutral-800 rounded-lg"}>
                         <img src={"/svg/neuralnetwork.svg"} />
                     </div>
+                </div>
+                <hr className={"border border-dark border-solid"} />
+                <div className={"bg-neutral-800 pt-24"}>
+
                 </div>
             </div>
         </div>
