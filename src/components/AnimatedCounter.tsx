20c6ee06382a48f67d9300cdf17bcbd865819209
Tyler St-Amour <tylerstamour1@gmail.com>
2022-09-25 15:31:39 -0400

    Added color mixing and text to impact page

--- src/components/AnimatedCounter.tsx
+++ src/components/AnimatedCounter.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import hexRgb from "hex-rgb";
 
 interface Props {
     value: number;
@@ -6,10 +7,24 @@ interface Props {
     className?: string;
     showDecimals?: boolean;
     decimalPlaces?: number;
+    colorGradient?: string; // only hex at the moment
 }
 
-export default ({ value, time, className, showDecimals, decimalPlaces }: Props) => {
-    const [ count, setCount ] = useState(0);
+interface Variables {
+    count: number;
+    color: string;
+}
+
+const increaseColor = (color: string, baseColor: string, percentage: number): string => {
+    const rgb = hexRgb(color);
+    const base = hexRgb(baseColor);
+    return `rgb(${base.red * (1 - percentage) + rgb.red * percentage}, 
+    ${base.green * (1 - percentage) + rgb.green * percentage}, 
+    ${base.blue * (1 - percentage) + rgb.blue * percentage})`
+}
+
+export default ({ value, time, className, showDecimals, decimalPlaces, colorGradient }: Props) => {
+    const [ variables, setVariables ] = useState<Variables>({ count: 0, color: "white"});
 
     if (decimalPlaces !== undefined) {
         showDecimals = true;
@@ -25,17 +40,21 @@ export default ({ value, time, className, showDecimals, decimalPlaces }: Props) 
         const interval = setInterval(() => {
             const x = i * increase;
             const newCount = a * x * (x - 2 * x);
-            setCount(value * (newCount / max))
             if (i >= (time / increase)) {
                 clearTimeout(interval);
             }
             i++;
-        }, increase * 1000)
-    }, [ setCount ])
+            setVariables({
+                count: value * (newCount / max),
+                color: colorGradient !== undefined ? increaseColor(colorGradient, "#FFFFFF", newCount / max) : "",
+            });
 
+        }, increase * 1000)
+    }, [ setVariables ])
+    console.log(variables.color)
     return (
-        <p className={className}>
-            {showDecimals ? decimalPlaces !== undefined ? count.toFixed(decimalPlaces) : count : Math.trunc(count)}
+        <p className={className} style={{ color: variables.color }}>
+            {showDecimals ? decimalPlaces !== undefined ? variables.count.toFixed(decimalPlaces) : variables.count : Math.trunc(variables.count)}
         </p>
     )
 }
--- src/pages/Impact.tsx
+++ src/pages/Impact.tsx
@@ -1,6 +1,7 @@
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import AnimatedCounter from "../components/AnimatedCounter";
+import React from "react";
 
 
 export default () => (
@@ -10,63 +11,78 @@ export default () => (
             <p className={"text-4xl text-white mb-2"}>Impacte sur la société</p>
             <p className={"text-md text-gray-400 max-w-3xl text-center"}>L'Intelligence artificiel, comme les réseaux neuronaux, on un impacte sur la society de plus en plus large. Mais voyons comment ils impacte la production de musique. </p>
         </div>
-        <p className={"text-3xl text-white flex justify-center mb-4"}>
+        <p className={"text-3xl text-white flex justify-center mb-8"}>
             Quelques Statistiques
         </p>
-        <div className={"flex flex-col sm:grid sm:grid-cols-2 gap-6 ml-24 mr-24 mb-6"}>
-            <div className={"flex flex-col items-center"}>
-                <div className={"flex flex-row mb-1"}>
-                    <AnimatedCounter value={62} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"}/>
-                    <p className={"text-gray-400 text-md flex items-end ml-2"}>Milliards</p>
+        <div className={"flex justify-center"}>
+            <div className={"flex flex-col sm:grid sm:grid-cols-2 gap-12 gap-x-32 mb-14"}>
+                <div className={"flex flex-col items-center"}>
+                    <div className={"flex flex-row mb-1"}>
+                        <AnimatedCounter value={62} time={5} decimalPlaces={2} showDecimals={true} className={"font-semibold text-3xl"} colorGradient={"#9932CC"}/>
+                        <p className={"text-gray-400 text-md flex items-end ml-2"}>Milliards</p>
+                    </div>
+                    <p className={"text-gray-400"}>
+                        Valeur de l'industrie musicale
+                    </p>
                 </div>
-                <p className={"text-gray-400"}>
-                    Valeur de l'industrie musicale
-                </p>
+                <div className={"flex flex-col items-center"}>
+                    <div className={"flex flex-row mb-1"}>
+                        <AnimatedCounter value={422.37} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
+                        <p className={"text-gray-400 text-md flex items-end ml-2"}>Milliards</p>
+                    </div>
+                    <p className={"text-gray-400"}>
+                        Valeur de l'industrie IA
+                    </p>
+                </div>
+                <div className={"flex flex-col items-center"}>
+                    <div className={"flex flex-row mb-1"}>
+                        <AnimatedCounter value={6.} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
+                        <p className={"text-gray-400 text-md flex items-end ml-1"}>%</p>
+                    </div>
+                    <p className={"text-gray-400"}>
+                        Croissance expecté jusqu'à 2030
+                    </p>
+                </div>
+                <div className={"flex flex-col items-center"}>
+                    <div className={"flex flex-row mb-1"}>
+                        <AnimatedCounter value={39.4} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
+                        <p className={"text-gray-400 text-md flex items-end ml-1"}>%</p>
+                    </div>
+                    <p className={"text-gray-400"}>
+                        Croissance expecté jusqu'à 2028
+                    </p>
+                </div>
+                <div className={"flex flex-col items-center"}>
+                    <div className={"flex flex-row mb-1"}>
+                        <AnimatedCounter value={350} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
+                        <p className={"text-gray-400 text-md flex items-end ml-2"}>Milles</p>
+                    </div>
+                    <p className={"text-gray-400"}>
+                        Nombres d'emplois dans l'industrie musicale
+                    </p>
+                </div>
+                <div className={"flex flex-col items-center"}>
+                    <div className={"flex flex-row mb-1"}>
+                        <AnimatedCounter value={300.999} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
+                        <p className={"text-gray-400 text-md flex items-end ml-2"}>Milles</p>
+                    </div>
+                    <p className={"text-gray-400"}>
+                        Nombre d'emplois dans l'industrie IA
+                    </p>
+                </div>
             </div>
-            <div className={"flex flex-col items-center"}>
-                <div className={"flex flex-row mb-1"}>
-                    <AnimatedCounter value={422.37} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"}/>
-                    <p className={"text-gray-400 text-md flex items-end ml-2"}>Milliards</p>
+        </div>
+        <hr className={"border border-solid border-dark"}/>
+        <div className={"flex flex-col bg-neutral-800"}>
+            <div className={"flex flex-col sm:grid sm:grid-cols-2 sm:gap-10 bg-neutral-800 pt-20 pl-10 sm:pl-20 pr-10 sm:pr-20 pb-24"}>
+                <div className={"pb-12 sm:pb-0"}>
+                    <p className={"text-white text-xl sm:text-2xl"}>Impacte sur les embauches</p>
+                    <p className={"text-gray-400 mt-2"}>
+                        Les intelligences artificielles, comme Jukebox, sont très utile pour la composition de musique. Tandis qu'ils ne sont pas parfait au moment, ils peuvent encore nous aider dans le processus. Alors, certaines embauches dans l'industrie de la musique vont disparaître en faveur de l'automation. Par exemple, un dévéloppeur indépendent n'aurais pas besoin d'embaucher un musicien pour la musique d'un jeu, plutôt, il pourra le genéré avec un réseau neuronal. Mais, lorsqu'il y a des embauches perdu, il y a toujours d'autres embauches créer. Donc on va continuer a observer la croissance des emplois concernent l'intelligence artificielle.                    </p>
                 </div>
-                <p className={"text-gray-400"}>
-                    Valeur de l'industrie IA
-                </p>
-            </div>
-            <div className={"flex flex-col items-center"}>
-                <div className={"flex flex-row mb-1"}>
-                    <AnimatedCounter value={6.} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"}/>
-                    <p className={"text-gray-400 text-md flex items-end ml-2"}>%</p>
+                <div className={"flex h-52 w-full sm:w-auto self-center justify-self-center bg-neutral-900 rounded-lg border-dark border-solid border-[1px]"}>
+                    <img src={"/svg/neuralnetwork.svg"} />
                 </div>
-                <p className={"text-gray-400"}>
-                    Croissance expecté jusqu'à 2030
-                </p>
-            </div>
-            <div className={"flex flex-col items-center"}>
-                <div className={"flex flex-row mb-1"}>
-                    <AnimatedCounter value={39.4} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"}/>
-                    <p className={"text-gray-400 text-md flex items-end ml-2"}>%</p>
-                </div>
-                <p className={"text-gray-400"}>
-                    Croissance expecté jusqu'à 2028
-                </p>
-            </div>
-            <div className={"flex flex-col items-center"}>
-                <div className={"flex flex-row mb-1"}>
-                    <AnimatedCounter value={350} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"}/>
-                    <p className={"text-gray-400 text-md flex items-end ml-2"}>Milles</p>
-                </div>
-                <p className={"text-gray-400"}>
-                    Nombres d'emplois dans l'industrie musicale 
-                </p>
-            </div>
-            <div className={"flex flex-col items-center"}>
-                <div className={"flex flex-row mb-1"}>
-                    <AnimatedCounter value={300.999} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"}/>
-                    <p className={"text-gray-400 text-md flex items-end ml-2"}>Milles</p>
-                </div>
-                <p className={"text-gray-400"}>
-                    Nombre d'emplois dans l'industrie IA
-                </p>
             </div>
         </div>
         <Footer/>
