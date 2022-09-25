d77ab78cf6a84032491529869c5b98f5189a9d44
Tyler St-Amour <tylerstamour1@gmail.com>
2022-09-25 16:26:45 -0400

    Impact page

--- src/pages/Impact.tsx
+++ src/pages/Impact.tsx
@@ -80,9 +80,20 @@ export default () => (
                     <p className={"text-gray-400 mt-2"}>
                         Les intelligences artificielles, comme Jukebox, sont très utile pour la composition de musique. Tandis qu'ils ne sont pas parfait au moment, ils peuvent encore nous aider dans le processus. Alors, certaines embauches dans l'industrie de la musique vont disparaître en faveur de l'automation. Par exemple, un dévéloppeur indépendent n'aurais pas besoin d'embaucher un musicien pour la musique d'un jeu, plutôt, il pourra le genéré avec un réseau neuronal. Mais, lorsqu'il y a des embauches perdu, il y a toujours d'autres embauches créer. Donc on va continuer a observer la croissance des emplois concernent l'intelligence artificielle.                    </p>
                 </div>
-                <div className={"flex h-52 w-full sm:w-auto self-center justify-self-center bg-neutral-900 rounded-lg border-dark border-solid border-[1px]"}>
-                    <img src={"/svg/neuralnetwork.svg"} />
+                <div className={"flex h-52 w-full pr-2 pl-2 sm:w-auto self-center justify-self-center bg-neutral-900 rounded-lg border-dark border-solid border-[1px]"}>
+                    <img src={"/svg/graph.svg"} />
                 </div>
+            </div>
+        </div>
+        <div className={"flex flex-col sm:grid sm:grid-cols-2 sm:gap-10 bg-neutral-900 pt-24 2xl:pt-24 pl-10 sm:pl-20 pr-10 sm:pr-20 pb-12"}>
+            <div className={"flex h-52 w-full sm:w-auto self-center justify-self-center bg-neutral-800 rounded-lg border-dark border-solid border-[1px]"}>
+                <img src={"/svg/chip.svg"} />
+            </div>
+            <div className={"pb-12 sm:pb-0"}>
+                <p className={"text-white text-xl sm:text-2xl"}>Impacte sur les conditions de travail</p>
+                <p className={"text-gray-400 mt-2"}>
+                    Avec l'innovation de Jukebox, Aiva, Boomy, Soundraw, et d'autres réseau neuronal, la tâche d'un musicien ou compositeur va devenir beaucoup plus simple. Ils vont dépensé plus de temps à écrire une description pour que l'IA puissent le transformé en musique. On pourra aussi utiliser les compositions genérer par un réseau comme modèle ou inspiration pour un autre oeuvre. Mais, la tâches des chanteurs ne changerait pas pour au moins quelques années parce que ces réseau ne sont pas capable de produire une voie humaine et compréhensible.
+                </p>
             </div>
         </div>
         <Footer/>
