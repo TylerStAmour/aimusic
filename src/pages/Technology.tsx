18561b1bf82544d3d99fdec7064ad5dc46b1dcc7
Tyler St-Amour <tylerstamour1@gmail.com>
2022-09-25 23:11:17 -0400

    Added more technology sections

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -6,70 +6,46 @@ import React from "react";
 export default () => (
     <div className={"flex flex-col bg-neutral-900"}>
         <Navigation />
-        <div className={"flex flex-col items-center mb-48"}>
+        <div className={"flex flex-col items-center mb-16"}>
             <p className={"text-4xl text-white mb-2"}>Les Réseaux Neuronaux</p>
             <p className={"text-md text-gray-400 max-w-3xl text-center"}>L'Intelligence artificiel, comme les réseaux neuronaux, on un impacte sur la society de plus en plus large. Mais voyons comment ils impacte la production de musique. </p>
         </div>
-        <div className={"flex flex-col items-center"}>
-            <p className={"text-3xl text-white flex justify-center mb-8"}>
-                Quelques Statistiques
+        <hr className={"border border-solid border-dark"}/>
+        <div className={"flex flex-col items-center pt-12 bg-neutral-800"}>
+            <p className={"text-3xl text-white"}>GPT-3 par OpenAI</p>
+            <p className={"text-gray-400 text-md max-w-3xl pb-6 pt-2"}>
+                GPT-3 est l'un des peu réseaux neuronaux qui est open-source et accessible par le publique. Son but est d'utiliser la méthode « deep learning » pour produire du texte qui resemble un humain. Il consiste d'un réseau transformateur avec une longeur de 2048 « tokens. » Dans ce cas, un « token » est comparable à un mot. Voici un exemple ci-dessous:
             </p>
-            <p className={"text-white"}>(ignore the statistics for now, I'll be changing that later)</p>
+            <img src={"tokens.png"} className={"rounded-lg"}/>
+            <p className={"text-gray-400 text-xs pb-6 pt-2"}>
+                Vous pouvez tester cette méthode de « token » pour vous même avec <a href={"https://beta.openai.com/tokenizer"} target={"_blank"} className={"text-secondary underline"}>cette outil</a> créer par OpenAI.
+            </p>
+            <p className={"text-gray-400 text-md max-w-3xl pb-12"}>
+                Ce réseau est considérer comme une innovation énorme dans le domaine de traitement du language naturel parce qu'il peut créer des textes incroyablement réaliste. Pour atteindre cela, il a nécessité près de <span className={"text-secondary"}>500 milliards</span> « tokens. » La majorité de ces « tokens » ont été obtenu en traversant l'internet et en collectant des données, mais d'autres sont venu des livres ou de l'archive Wikipédia.
+            </p>
         </div>
+        <hr className={"border border-solid border-dark"}/>
+        <p className={"text-3xl text-white flex justify-center pt-8 mb-8"}>
+            Statistiques sur GPT-3
+        </p>
         <div className={"flex justify-center"}>
             <div className={"flex flex-col sm:grid sm:grid-cols-2 gap-12 gap-x-32 mb-14"}>
                 <div className={"flex flex-col items-center"}>
                     <div className={"flex flex-row mb-1"}>
-                        <AnimatedCounter value={62} time={5} decimalPlaces={2} showDecimals={true} className={"font-semibold text-3xl"} colorGradient={"#9932CC"}/>
+                        <AnimatedCounter value={175} time={3} decimalPlaces={2} showDecimals={true} className={"font-semibold text-3xl"} colorGradient={"#9932CC"}/>
                         <p className={"text-gray-400 text-md flex items-end ml-2"}>Milliards</p>
                     </div>
                     <p className={"text-gray-400"}>
-                        Valeur de l'industrie musicale
+                        Nombre de paramètres
                     </p>
                 </div>
                 <div className={"flex flex-col items-center"}>
                     <div className={"flex flex-row mb-1"}>
-                        <AnimatedCounter value={422.37} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
-                        <p className={"text-gray-400 text-md flex items-end ml-2"}>Milliards</p>
+                        <AnimatedCounter value={800} time={3} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
+                        <p className={"text-gray-400 text-md flex items-end ml-2"}>GBs</p>
                     </div>
                     <p className={"text-gray-400"}>
-                        Valeur de l'industrie IA
-                    </p>
-                </div>
-                <div className={"flex flex-col items-center"}>
-                    <div className={"flex flex-row mb-1"}>
-                        <AnimatedCounter value={6.} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
-                        <p className={"text-gray-400 text-md flex items-end ml-1"}>%</p>
-                    </div>
-                    <p className={"text-gray-400"}>
-                        Croissance expecté jusqu'à 2030
-                    </p>
-                </div>
-                <div className={"flex flex-col items-center"}>
-                    <div className={"flex flex-row mb-1"}>
-                        <AnimatedCounter value={39.4} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
-                        <p className={"text-gray-400 text-md flex items-end ml-1"}>%</p>
-                    </div>
-                    <p className={"text-gray-400"}>
-                        Croissance expecté jusqu'à 2028
-                    </p>
-                </div>
-                <div className={"flex flex-col items-center"}>
-                    <div className={"flex flex-row mb-1"}>
-                        <AnimatedCounter value={350} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
-                        <p className={"text-gray-400 text-md flex items-end ml-2"}>Milles</p>
-                    </div>
-                    <p className={"text-gray-400"}>
-                        Nombres d'emplois dans l'industrie musicale
-                    </p>
-                </div>
-                <div className={"flex flex-col items-center"}>
-                    <div className={"flex flex-row mb-1"}>
-                        <AnimatedCounter value={300.999} time={5} decimalPlaces={2} showDecimals={true} className={"text-white font-semibold text-3xl"} colorGradient={"#9932CC"}/>
-                        <p className={"text-gray-400 text-md flex items-end ml-2"}>Milles</p>
-                    </div>
-                    <p className={"text-gray-400"}>
-                        Nombre d'emplois dans l'industrie IA
+                        Montant de GBs nécessaire pour GPT-3
                     </p>
                 </div>
             </div>
