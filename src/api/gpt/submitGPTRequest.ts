40d29b1e8ac074e2409a926dba6daac151698bb1
Tyler St-Amour <tylerstamour1@gmail.com>
2022-09-26 22:57:10 -0400

    Forgot to resolve promise

--- src/api/gpt/submitGPTRequest.ts
+++ src/api/gpt/submitGPTRequest.ts
@@ -1,5 +1,9 @@
 import http from '../http';
 
 export default async (prompt: string): Promise<string> => {
-    return await http.post("https://tyler.st/api/gpt/prompt", { prompt: prompt })
+    return await new Promise((resolve, reject) => {
+        http.post("https://tyler.st/api/gpt/prompt", { prompt: prompt })
+            .then((data) => resolve(data.data))
+            .catch(reject)
+    })
 }
